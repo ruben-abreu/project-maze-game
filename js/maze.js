a2d527d935da9ec13ce124c1c708e772eb17ebfb
anapatriciagomes <anapatrigomes@gmail.com>
2023-11-01 19:02:32 +0000

    added images and maze arrays to js

---
+++ js/maze.js
@@ -0,0 +1,10 @@
+class MazeGame {
+  constructor(map, level) {
+    this.level = level;
+    this.map = map;
+    this.playerIcon = playerIcon;
+    this.endGoal = endGoal;
+    this.poison = poison;
+    this.potion = potion;
+  }
+}
