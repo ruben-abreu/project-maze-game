f08c99743ef6b562da6f03568b6356a8cfb610da
anapatriciagomes <anapatrigomes@gmail.com>
2023-11-02 17:06:07 +0000

    game tile size adjustment

--- js/script.js
+++ js/script.js
@@ -148,14 +148,14 @@ for (let i = 0; i < levels.length; i++) {
         tile.classList.add('start');
         let startImage = document.createElement('img');
         startImage.src = '/images/harry-potter.png';
-        startImage.width = '46';
+        startImage.width = '26';
         tile.appendChild(startImage);
       } else if (levels[i].tiles[r][c] === 3) {
         tile.classList.add('path');
         tile.classList.add('end');
         let endImage = document.createElement('img');
         endImage.src = '/images/golden-snitch.png';
-        endImage.width = '46';
+        endImage.width = '26';
         tile.appendChild(endImage);
       }
       if (i === 0) {
