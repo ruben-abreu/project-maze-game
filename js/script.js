25b4a21fd115dd396ed33e1a46eb73bfc19fd141
Ruben M. Abreu <ruben_abreu@icloud.com>
2023-11-04 15:32:59 +0000

    fixing Timer issues

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 window.onload = function () {
   const select = document.getElementById('difficulty-dropdown');
-  const selectValue = select.value;
   const playButton = document.getElementById('play-button');
   const game = new MazeGame();
   const timerDisplay = document.getElementById('timer');
-  const timer = new Timer(selectValue, timerDisplay);
+  let timer;
 
-  select.addEventListener('change', function () {
-    game.levelSelection();
-
+  function updateTimer() {
+    const selectValue = select.value;
     if (timer) {
       timer.stopTimer();
     }
+    timer = new Timer(selectValue, timerDisplay);
     timer.startTimer();
+  }
+
+  select.addEventListener('change', function () {
+    game.levelSelection();
+    updateTimer();
   });
 
   playButton.addEventListener('click', function () {
     game.play();
     game.hidePlayButton();
     game.tryAgain();
-
     if (timer) {
       timer.stopTimer();
     }
-    timer.startTimer();
+    updateTimer();
   });
 };
