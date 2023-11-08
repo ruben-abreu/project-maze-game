fc23e8f510a992ea570581d56f4068b502a93a8e
Ruben M. Abreu <ruben_abreu@icloud.com>
2023-11-08 01:15:11 +0000

    Timer working as expected, script.js cleanup

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ window.onload = function () {
   const playButton = document.getElementById('play-button');
   const game = new MazeGame();
   const timerDisplay = document.getElementById('timer');
-  let timer;
   const mobileUpButton = document.getElementById('up');
   const mobileDownButton = document.getElementById('down');
   const mobileRightButton = document.getElementById('right');
@@ -11,14 +10,7 @@ window.onload = function () {
   let c;
   const continueButton = document.getElementById('continue-button');
   const playAgainButton = document.getElementById('play-again-button');
-
-  function updateTimer() {
-    if (timer) {
-      timer.stopTimer();
-    }
-    timer = new Timer(timerDisplay);
-    timer.startTimer();
-  }
+  const timer = new Timer(timerDisplay);
 
   playButton.addEventListener('click', function () {
     game.play();
@@ -26,7 +18,7 @@ window.onload = function () {
     game.move();
     game.hidePlayButton();
     game.reset();
-    updateTimer();
+    timer.startTimer();
   });
 
   continueButton.addEventListener('click', function () {
